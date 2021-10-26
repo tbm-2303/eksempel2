402b6e66059d44d9e378c0a532b6156be56de5ca
timmy <kris>
2021-10-26 17:53:29 +0200

    .

--- src/main/java/com/example/eksempel2/HelloServlet.java
+++ src/main/java/com/example/eksempel2/HelloServlet.java
@@ -1,6 +1,7 @@
 package com.example.eksempel2;
 
 import java.io.*;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
@@ -9,10 +10,21 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "This is the servlet. Welcome";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
 
         // Hello
